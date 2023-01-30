2d95df4ce0dc6eb5d68990720e4591ded510dab3
yaw.o.k <obengky72@gmail.com>
2023-01-30 23:35:26 +0000

    Format with black

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,40 +6,52 @@ from django_countries.widgets import CountrySelectWidget
 class BookInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
-    
-    
+
+
 class BookInline(admin.TabularInline):
     model = Book
     extra = 0
-    fields = ('title', 'isbn', 'language')
-    
-    
+    fields = ("title", "isbn", "language")
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth','date_of_death')
-    fields = ['first_name', 'last_name', 'nationality', ('date_of_birth', 'date_of_death'),]
-    widgets = {'nationality': CountrySelectWidget()}
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+    fields = [
+        "first_name",
+        "last_name",
+        "nationality",
+        ("date_of_birth", "date_of_death"),
+    ]
+    widgets = {"nationality": CountrySelectWidget()}
     inlines = [BookInline]
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
+    list_display = ("title", "author", "display_genre")
     inlines = [BookInstanceInline]
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'borrower', 'due_back', 'is_overdue', 'id')
-    list_filter = ('status', 'due_back')
-    
+    list_display = ("book", "status", "borrower", "due_back", "is_overdue", "id")
+    list_filter = ("status", "due_back")
+
     fieldsets = (
-        (None, {
-            'fields' : ('book', 'imprint', 'id') 
-        }),
-        ('Availability', {
-            'fields' : ('status', 'due_back', 'borrower',)
-        }),
+        (None, {"fields": ("book", "imprint", "id")}),
+        (
+            "Availability",
+            {
+                "fields": (
+                    "status",
+                    "due_back",
+                    "borrower",
+                )
+            },
+        ),
     )
-    
-    
+
+
 admin.site.register(Language)
 admin.site.register(Genre)
