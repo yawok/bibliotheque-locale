29e7d5e1518721641a414f4b947249c8cd5b4d82
yaw.o.k <obengky72@gmail.com>
2023-01-30 23:34:55 +0000

    Format with black

--- catalog/models.py
+++ catalog/models.py
@@ -12,112 +12,122 @@ from datetime import date
 
 class Genre(models.Model):
     """A model for Book Genre."""
-    name = models.CharField(max_length=255, help_text="Enter a book genre.(Eg: Science Fiction)")
-    
-    
+
+    name = models.CharField(
+        max_length=255, help_text="Enter a book genre.(Eg: Science Fiction)"
+    )
+
     def __str__(self):
         return self.name
-    
-    
+
+
 class Book(models.Model):
     """A model for Book"""
-    title = models.CharField(max_length=255, help_text="Enter a book title")
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-    isbn = models.CharField("ISBN", max_length=13, unique=True, help_text="13 Character <a href='https://www.isbn-international.org/content/what-isbn'>ISBN number</a>")
-    genre = models.ManyToManyField(Genre, help_text="Select a book genre")
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
-    
+    title = models.CharField(max_length=255, help_text="Enter a book title")
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the book"
+    )
+    isbn = models.CharField(
+        "ISBN",
+        max_length=13,
+        unique=True,
+        help_text="13 Character <a href='https://www.isbn-international.org/content/what-isbn'>ISBN number</a>",
+    )
+    genre = models.ManyToManyField(Genre, help_text="Select a book genre")
+    language = models.ForeignKey("Language", on_delete=models.SET_NULL, null=True)
+
     def __str__(self):
         return self.title
-    
-    
+
     def display_genre(self):
         """Create a list of the book's genre required for display in the Admin interface."""
-        return ', '.join(genre.name for genre in self.genre.all()[:3])
-    
-    
+        return ", ".join(genre.name for genre in self.genre.all()[:3])
+
     def get_absolute_url(self):
         return reverse("book-detail", args=[str(self.id)])
-    
+
     class Meta:
-        ordering = ['title']
-        permissions = (('can_add_book', 'Can add new book'),)
-        
-    
+        ordering = ["title"]
+        permissions = (("can_add_book", "Can add new book"),)
+
+
 class BookInstance(models.Model):
     """A model for a book instance."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID of this book for the whole library.")
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text="Unique ID of this book for the whole library.",
+    )
     book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=255)
     due_back = models.DateField(null=True, blank=True, help_text="YYYY-MM-DD")
     LOAN_STATUS = (
-        ('m', 'Maintainance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
+        ("m", "Maintainance"),
+        ("o", "On loan"),
+        ("a", "Available"),
+        ("r", "Reserved"),
     )
     status = models.CharField(
         max_length=1,
         choices=LOAN_STATUS,
         blank=True,
-        default='m',
-        help_text='Book availability',
+        default="m",
+        help_text="Book availability",
     )
     borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    
-    
+
     class Meta:
-        ordering = ['due_back']
+        ordering = ["due_back"]
         permissions = (("can_mark_returned", "Set book as returned"),)
-        
-    
-    @admin.display(
-        boolean=True,
-        ordering='due_back',
-        description='Over Due?'
-    )
+
+    @admin.display(boolean=True, ordering="due_back", description="Over Due?")
     def is_overdue(self):
         """Check if book instance is overdue."""
         return bool(self.due_back and (date.today() > self.due_back))
-    
+
     def __str__(self):
         return f"{self.id} ({self.book.title})"
-    
-    
-    
+
+
 class Author(models.Model):
     """A model for Auther."""
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    nationality = CountryField(multiple=True, blank_label='(select country)', help_text="Hold down “Control”, or “Command” on a Mac, to select more than one." )
+    nationality = CountryField(
+        multiple=True,
+        blank_label="(select country)",
+        help_text="Hold down “Control”, or “Command” on a Mac, to select more than one.",
+    )
     date_of_birth = models.DateField(null=True, blank=True, help_text="YYYY-MM-DD")
-    date_of_death = models.DateField('died', null=True, blank=True, help_text="YYYY-MM-DD")
-    
-    
-    
+    date_of_death = models.DateField(
+        "died", null=True, blank=True, help_text="YYYY-MM-DD"
+    )
+
     class Meta:
-        ordering = ['last_name', 'first_name']
-        
-    
+        ordering = ["last_name", "first_name"]
+
     def get_absolute_url(self):
         return reverse("author-detail", args=[str(self.id)])
-    
 
     def get_full_name(self):
         """Return author's full name"""
-        return f'{self.last_name}, {self.first_name}'
-    
+        return f"{self.last_name}, {self.first_name}"
+
     def __str__(self):
-        return f'{self.last_name}, {self.first_name}'
-    
-    
+        return f"{self.last_name}, {self.first_name}"
+
+
 class Language(models.Model):
     """A model for languages."""
-    name = models.CharField(max_length=255, help_text="Enter the book's natural language(Eg: English, French")
-    
-    
+
+    name = models.CharField(
+        max_length=255,
+        help_text="Enter the book's natural language(Eg: English, French",
+    )
+
     def __str__(self):
         return self.name
-    
--- catalog/views.py
+++ catalog/views.py
@@ -18,125 +18,131 @@ def index(request):
     """Returns the counts of books, book instances and authors."""
     number_of_books = Book.objects.all().count()
     number_of_book_instances = BookInstance.objects.all().count()
-    number_of_available_book_instances = BookInstance.objects.filter(status__exact='a').count()
+    number_of_available_book_instances = BookInstance.objects.filter(
+        status__exact="a"
+    ).count()
     number_of_authors = Author.objects.all().count()
-    books_containing_of = Book.objects.filter(title__icontains='of').count()
-    number_of_visits = request.session.get('number_of_visits', 0)
-    request.session['number_of_visits'] = number_of_visits + 1 
-    
+    books_containing_of = Book.objects.filter(title__icontains="of").count()
+    number_of_visits = request.session.get("number_of_visits", 0)
+    request.session["number_of_visits"] = number_of_visits + 1
+
     context = {
-        'number_of_books': number_of_books,
-        'number_of_book_instances': number_of_book_instances,
-        'number_of_available_book_instances': number_of_available_book_instances,
-        'number_of_authors': number_of_authors,
-        'books_containing_of': books_containing_of,
-        'number_of_visits': number_of_visits,
+        "number_of_books": number_of_books,
+        "number_of_book_instances": number_of_book_instances,
+        "number_of_available_book_instances": number_of_available_book_instances,
+        "number_of_authors": number_of_authors,
+        "books_containing_of": books_containing_of,
+        "number_of_visits": number_of_visits,
     }
-    
-    return render(request, 'catalog/index.html', context)
+
+    return render(request, "catalog/index.html", context)
 
 
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
-    
-    
+
+
 class BookDetailView(generic.DetailView):
     model = Book
     paginate_by = 10
-    
-    
+
+
 class AuthorListView(generic.ListView):
     model = Author
     paginate_by = 10
-    
-    
+
+
 class AuthorDetailView(generic.DetailView):
     model = Author
 
-    
+
 class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """View to display books loaded to user"""
+
     model = BookInstance
-    template_name = 'catalog/books_borrowed_by_user.html'
+    template_name = "catalog/books_borrowed_by_user.html"
     paginate_by = 10
-    
+
     def get_queryset(self):
-        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
-    
-    
-    
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact="o")
+            .order_by("due_back")
+        )
+
+
 class BorrowedListView(PermissionRequiredMixin, generic.ListView):
-    permission_required = 'catalog.can_mark_returned'
+    permission_required = "catalog.can_mark_returned"
     model = BookInstance
-    template_name = 'catalog/all_borrowed_list.html'
+    template_name = "catalog/all_borrowed_list.html"
     paginate_by = 10
-    
-    
+
     def get_queryset(self):
-        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
-    
+        return BookInstance.objects.filter(status__exact="o").order_by("due_back")
+
+
 @login_required
-@permission_required('catalog.can_mark_returned', raise_exception=True)   
+@permission_required("catalog.can_mark_returned", raise_exception=True)
 def renew_book_labrarian(request, pk):
     book_instance = get_object_or_404(BookInstance, pk=pk)
-    
-    if request.method == 'POST':
-        form = RenewBookModelForm(request.POST) 
+
+    if request.method == "POST":
+        form = RenewBookModelForm(request.POST)
 
         if form.is_valid():
-            book_instance.due_back = form.cleaned_data['due_back']
+            book_instance.due_back = form.cleaned_data["due_back"]
             book_instance.save()
-            
-            return HttpResponseRedirect(reverse('borrowed'))
+
+            return HttpResponseRedirect(reverse("borrowed"))
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookModelForm(initial={
-            'due_back': proposed_renewal_date
-        })
+        form = RenewBookModelForm(initial={"due_back": proposed_renewal_date})
 
-    context = {
-            'form': form,
-            'book_instance': book_instance
-        }
+    context = {"form": form, "book_instance": book_instance}
 
-    return render(request, 'catalog/book_renew_librarian.html', context)
+    return render(request, "catalog/book_renew_librarian.html", context)
+
 
 class AuthorCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
-    permission_required = 'catalog.can_mark_returned'
+    permission_required = "catalog.can_mark_returned"
     model = Author
-    fields = ['first_name', 'last_name', 'nationality', 'date_of_birth', 'date_of_death']
-    initial = {'date_of_birth': '2000-01-01'}
-    widgets = {'nationality': CountrySelectWidget()}
-    
+    fields = [
+        "first_name",
+        "last_name",
+        "nationality",
+        "date_of_birth",
+        "date_of_death",
+    ]
+    initial = {"date_of_birth": "2000-01-01"}
+    widgets = {"nationality": CountrySelectWidget()}
 
-    
+
 class AuthorUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
-    permission_required = 'catalog.can_mark_returned'
+    permission_required = "catalog.can_mark_returned"
     model = Author
-    fields = '__all__'
+    fields = "__all__"
 
-    
+
 class AuthorDelete(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
-    permission_required = 'catalog.can_mark_returned'
+    permission_required = "catalog.can_mark_returned"
     model = Author
-    success_url = reverse_lazy('authors')
-    
-    
-class BookCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
-    permission_required = 'catalog.can_add_book'
-    model = Book
-    fields = '__all__'
+    success_url = reverse_lazy("authors")
 
-    
-    
+
+class BookCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    permission_required = "catalog.can_add_book"
+    model = Book
+    fields = "__all__"
+
+
 class BookUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
-    permission_required = 'catalog.can_add_book'
+    permission_required = "catalog.can_add_book"
     model = Book
-    fields = '__all__'
-    
-    
+    fields = "__all__"
+
+
 class BookDelete(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
-    permission_required = 'catalog.can_add_book'
+    permission_required = "catalog.can_add_book"
     model = Book
-    success_url = reverse_lazy('books')
+    success_url = reverse_lazy("books")
