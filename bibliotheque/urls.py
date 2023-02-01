dfb58345b061e4be9d7e352185658456691bccc0
yaw.o.k <obengky72@gmail.com>
2023-02-01 23:42:20 +0000

    Format file

--- bibliotheque/urls.py
+++ bibliotheque/urls.py
@@ -21,10 +21,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("admin/", admin.site.urls),
+    path("catalog/", include("catalog.urls")),
+    path("", RedirectView.as_view(url="catalog/", permanent=True)),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
