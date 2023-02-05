91f75e061079da517c03ea4e885e23f0c042aa8c
yaw.o.k <obengky72@gmail.com>
2023-02-05 16:45:34 +0000

    Add migrations and ALLOWED_HOSTS

--- bibliotheque/settings.py
+++ bibliotheque/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "web-production-115a.up.railway.app" ]
 
 
 # Application definition
