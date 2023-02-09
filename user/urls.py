ea8790270786a09a7ba93a87268442ba6465e9f8
yaw.o.k <obengky72@gmail.com>
2023-02-09 23:45:22 +0000

    Add templates to user

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 
-urls = [
+urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("signup/", views.signup, name="signup"),
 ]
